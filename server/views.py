e33082f04408d3318f7461b1182210629aa2c1c1
Lucas Vieira <lucas.vieira.093@ufrn.edu.br>
2023-09-28 22:09:15 -0300

    Implementando campo num_members e sua visualização na resposta

--- server/views.py
+++ server/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.exceptions import AuthenticationFailed, ValidationError
@@ -15,6 +16,7 @@ class ServerListViewSet(viewsets.ViewSet):
         qty = request.query_params.get("qty")
         by_user = request.query_params.get("by_user") == "true"
         by_serverid = request.query_params.get("by_serverid")
+        with_num_members = request.query_params.get("num_members") == "true"
 
         if by_user or by_serverid and not request.user.is_authenticated:
             raise AuthenticationFailed()
@@ -25,6 +27,9 @@ class ServerListViewSet(viewsets.ViewSet):
         if by_user:
             user_id = request.user.id
             self.queryset = self.queryset.filter(member=user_id)
+
+        if with_num_members:
+            self.queryset = self.queryset.annotate(num_members=Count("member"))
         
         if qty:
             self.queryset = self.queryset[: int(qty)]
@@ -35,8 +40,8 @@ class ServerListViewSet(viewsets.ViewSet):
                 if not self.queryset.exists():
                     raise ValidationError(detail=f"Server with id {by_serverid} not found")
             except ValueError:
-                raise ValidationError(detail=f"Server value error")
+                raise ValidationError(detail="Server value error")
         
-        serializer = ServerSerializer(self.queryset, many=True)
+        serializer = ServerSerializer(self.queryset, many=True, context={"num_members": with_num_members})
         return Response(serializer.data)
 
