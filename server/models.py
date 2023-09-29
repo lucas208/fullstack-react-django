9d3b72918e1614807a820e3141702a30de0a00bc
Lucas Vieira <lucas.vieira.093@ufrn.edu.br>
2023-09-29 17:32:53 -0300

    Adicionando pillow e campos para imagens no model do Canal

--- server/models.py
+++ server/models.py
@@ -3,6 +3,11 @@ from django.db import models
 from django.dispatch import receiver
 from django.shortcuts import get_object_or_404
 
+def server_icon_upload_path(instance, filename):
+    return f"server/{instance.id}/server_icons/{filename}"
+
+def server_banner_upload_patch(instance, filename):
+    return f"server/{instance.id}/server_banner/{filename}"
 
 def category_icon_upload_path(instance, filename):
     return f"category/{instance.id}/category_icon/{filename}"
@@ -26,7 +31,7 @@ class Category(models.Model):
                 file = getattr(instance, field.name)
                 if file:
                     file.delete(save=False)
-                    
+
     def __str__(self):
         return self.name
 
@@ -45,10 +50,25 @@ class Channel(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="channel_owner")
     topic = models.CharField(max_length=100)
     server = models.ForeignKey(Server, on_delete=models.CASCADE, related_name="channel_server")
+    banner = models.ImageField(upload_to=server_banner_upload_patch, blank=True)
+    icon = models.ImageField(upload_to=server_icon_upload_path, blank=True)
 
     def save(self, *args, **kwargs):
-        self.name = self.name.lower()
-        super(Channel, self).save(*args, **kwargs)
+        if self.id:
+            existing = get_object_or_404(Server, id=self.id)
+            if existing.icon != self.icon:
+                existing.icon.delete(save=False)
+                super(Category, self).save(*args, **kwargs)
+            if existing.banner != self.banner:
+                existing.bannar.delete(save=False)
+    
+    @receiver(models.signals.pre_delete, sender="server.Server")
+    def category_delete_files(sender, instance, **kwargs):
+        for field in instance._meta.fields:
+            if field.name == "icon" or field.name == 'banner':
+                file = getattr(instance, field.name)
+                if file:
+                    file.delete(save=False)
 
     def __str__(self):
         return self.name
